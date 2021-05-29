8ac7a0bd424ff120df7c6cdee5016f58d75598d5
“NargizaRysbek” <r.nakosya@gmail.com>
2021-05-30 00:04:49 +0600

    added counter with react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Counter from './components/Counter'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import { counterReducer } from './store/reducers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const store = createStore(counterReducer)
+
+
+export const App = () => (
+  <Provider store={store}>
+     <Counter/>
+    
+  </Provider>
+  
+)
+
